81588f22ed27e9a13d0ea65b52491901ebdc2c25
Jahrel Stewart <jahrelstewart001@gmail.com>
2022-11-22 16:43:40 -0500

    homepage design complete

--- index.js
+++ index.js
@@ -44,12 +44,30 @@ class ValidateState {
 
 const validateState = new ValidateState({date: new Date().toLocaleString().split(",")[0], destination: ''});
 
+//-------------------------------------------------------date calender input...stuff :
+
 let destination = document.getElementById("whenFrom");
 destination.setAttribute("min", currDate());
+destination.setAttribute("max", maxDate());
 
 function currDate(){
    //yyyy-mm-dd
-   date = new Date().toLocaleString().split(",")[0];   
+   let date = new Date().toLocaleString().split(",")[0];     
+
+   return formatDate(date);
+}
+
+function maxDate(){
+  //yyyy-mm-dd
+  let date = currDate();
+  let dateSplit = date.split("-");
+  date = new Date(Number(dateSplit[0]), Number(dateSplit[1]), Number(dateSplit[2]));
+  date.setDate(date.getDate() + 1);// 15 days ahead is max for api  !!
+
+  return formatDate(date.toLocaleDateString());
+}
+
+function formatDate(date){    
    let day = date.split("/")[1];
    day = day.length < 2 ? "0".concat(day) : day;
    let month = date.split("/")[0];
@@ -59,19 +77,15 @@ function currDate(){
    return year.concat("-", month, "-", day);
 }
 
+
 //------------------------------------------------------- api location picker:
 $(function() {
    $('#map').locationpicker({
       location: {latitude: 46.15242437752303, longitude: 2.7470703125},   
       radius: 0,
       inputBinding: {
-      //   latitudeInput: $('#lat'),
-      //   longitudeInput: $('#lng'),
-         locationNameInput: $('#location')
+         locationNameInput: $('#autocomplete')
       },
       enableAutocomplete: true,
-   //    onchanged: function(currentLocation, radius, isMarkerDropped) {
-   //       alert("Location changed. New location (" + currentLocation.latitude + ", " + currentLocation.longitude + ")");
-   //     }
    }); 
 });
